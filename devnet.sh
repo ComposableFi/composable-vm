76de7703a29f1a1a1c8068572cce42d45ff66dd7
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-19 21:03:07 +0000

    happy

--- devnet.sh
+++ devnet.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-(
- cd node || exit
- clear && cargo run --bin mantis -- --centauri "http://localhost:26657" --osmosis "localhost:36657" --neutron "localhost:46657" --cvm-contract "centauri1" --wallet "mnemonic" --order-contract "centauri1"
-)
+#clear && cargo run --bin mantis -- --centauri "http://localhost:26657" --osmosis "localhost:36657" --neutron "localhost:46657" --cvm-contract "centauri1" --wallet "mnemonic" --order-contract "centauri1"
+
+RUST_TRACE=trace cargo run --bin mantis -- --centauri "https://rpc.composable.nodestake.top:443" --osmosis "todo" --neutron "todo" --cvm-contract "centauri1wpf2szs4uazej8pe7g8vlck34u24cvxx7ys0esfq6tuw8yxygzuqpjsn0d" --wallet "$WALLET" --order-contract "centauri1lnyecncq9akyk8nk0qlppgrq6yxktr68483ahryn457x9ap4ty2sthjcyt"
