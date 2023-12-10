1ac72f193100b3767f8eb370f6b1cf3b66a79387
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-10 14:21:07 +0000

    adding mapper around center

--- mantis/node/src/solver/router.rs
+++ mantis/node/src/solver/router.rs
@@ -1,4 +1,13 @@
 const MIN_RESERVE: f64 = 05000.0;
 const MAX_RESERVE: f64 = 15000.0;
 
+use std::collections::HashMap;
+
 use ndarray::*;
+use good_lp::*;
+use itertools::*;
+use prelude::*;
+
+pub fn populate_chain_map(chains: &mut HashMap<String, Vec<String>>, center_node: String) {
+
+}
