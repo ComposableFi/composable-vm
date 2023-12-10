3af3f3d3e5e5b1204eae7e2b36e6c070887ba9e5
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-10 18:55:59 +0000

    random of simulator

--- mantis/node/src/bin/optimal-routing.rs
+++ mantis/node/src/bin/optimal-routing.rs
@@ -20,9 +20,20 @@ fn main() {
 
     let mut all_tokens = vec![];
     let mut all_cfmms = vec![];
+    let mut reserves = vec![];
+    let mut cfmm_tx_cost = vec![];
     for (other_chain, other_tokens) in chains.clone() {
         all_tokens.extend(other_tokens.clone());
         let cfmms = other_tokens.clone().into_iter().combinations(2);
         all_cfmms.extend(cfmms);
+    }
+    use rand::prelude::*;
+    let tx_costs_random = rand_distr::Uniform::new(0, 20);
+    let reserves_radom = rand_distr::Uniform::new(9500, 10051);
+    for cfmm in all_cfmms {
+        let value = reserves_radom.sample(&mut rand::thread_rng());
+        reserves.push(value);
+        let value = tx_costs_random.sample(&mut rand::thread_rng());
+        cfmm_tx_cost.push(value);
     }
 }
