29a45d90bff46f5a85f0fa271e1bc41a413e0711
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-26 20:09:06 +0000

    real

--- mantis/simulation/data.py
+++ mantis/simulation/data.py
@@ -13,6 +13,7 @@ class AssetTransfers:
     out_asset_id: int
     
     # this is positive whole number too
+    # if it is hard if None, please fail if it is None - for now will be always some
     usd_fee_transfer: int | None
     
     # do not care
@@ -27,6 +28,7 @@ class AssetPairsXyk:
     fee_of_out_per_million: int 
     weight_of_a: int 
     weight_of_b: int 
+    # if it is hard if None, please fail if it is None - for now will be always some
     pool_value_in_usd: int | None
     a_amount: int
     b_amount: int
@@ -39,8 +41,9 @@ class Input:
     out_token_id: int
     in_amount: int
     out_amount: int
-    # if max is true, user wants to spent all in to get at least out
-    # if max is false, user wants to get exact out, but spent as small as possible in
+    # if max is True, user wants to spent all in to get at least out
+    # if max is False, user wants to get exact out, but spent as small as possible in
+    # please fail if bool is False for now
     max: bool
 
 class AllData():
