bbf039efb1d186c3eaa0784be7b617d02bd877e0
Alexey Chursinau <alekseyc@altisai.com>
2024-02-10 17:55:10 +0100

    Fix output data structure

--- mantis/blackbox/settings.py
+++ mantis/blackbox/settings.py
@@ -3,13 +3,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    osmosis_pools: str = Field(alias="OSMOSIS_POOLS")
-    composable_cosmos_grpc: str = Field(alias="COMPOSABLE_COSMOS_GRPC")
-    cvm_address: str = Field(alias="CVM_ADDRESS")
-    astroport_pools: str = Field(alias="ASTROPORT_POOLS")
+    osmosis_pools: str | None = Field(alias="OSMOSIS_POOLS", default=None)
+    composable_cosmos_grpc: str | None = Field(alias="COMPOSABLE_COSMOS_GRPC", default=None)
+    cvm_address: str | None = Field(alias="CVM_ADDRESS", default=None)
+    astroport_pools: str | None = Field(alias="ASTROPORT_POOLS", default=None)
     neutron_rpc: str | None = Field(alias="NEUTRON_RPC", default=None)
     osmosis_rpc: str | None = Field(alias="OSMOSIS_RPC", default=None)
-    skip_money: str = Field(alias="SKIP_MONEY")
+    skip_money: str | None = Field(alias="SKIP_MONEY", default=None)
     port: int = Field(default=8000, alias="LISTEN_PORT")
 
 
