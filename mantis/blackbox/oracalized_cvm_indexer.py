1217bf5b16da75974f88a0c99c7426a5c3866dcf
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-02-21 11:12:24 +0000

    finali bits outline

--- mantis/blackbox/oracalized_cvm_indexer.py
+++ mantis/blackbox/oracalized_cvm_indexer.py
@@ -6,24 +6,13 @@ from pydantic import BaseModel
 
 class Oracle(BaseModel):
     def from_usd(cvm: ExtendedCvmRegistry):
+        """_summary_
+            Builds USD oracle from data.
+        """
         pass
     
     def for_simulation():
+        """
+        Makes data exactly as it handled by simulation
+        """
         pass
-    def scale_in():
-        pass
-
-
-#     def to_cvm():
-#         """_summary_
-#         prodduce non CVMed XYK
-#         prodcue from CVM REG all IN OUT PAIRS of assets in good format
-#         filter out only CVMed with known assets
-#         replace with ids in XYK
-#         generate Transfers for all, find ga for comoss_chain_of each
-#         normalize all assets to oracle if ALL assets has USD.
-
-#         Produced CVMed data from raw chain data consumable by simulation
-#         """
-# ExtendedCvmRegistry
-#         pass
---
+++ mantis/simulation/routers/scaler.py
@@ -0,0 +1,14 @@
+
+# see https://or.stackexchange.com/questions/11603/numerical-infeasibility-for-moving-numbers-along-some-specific-conversion-edges
+
+    def scale_in():
+        """
+        Scales in data to be used by simulation
+        """
+        pass
+    
+    def scale_out():
+        """
+        Scales outs
+        """
+        pass    
