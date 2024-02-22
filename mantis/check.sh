3c4bdbc9db120cf8d45d55679f86d079c9162277
Alexey Chursinau <alekseyc@altisai.com>
2024-02-22 17:07:19 +0100

    Add server start status

--- mantis/check.sh
+++ mantis/check.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 poetry run pytest
-poetry run ruff check . --exit-non-zero-on-fix --fix-only --no-unsafe-fixes
+poetry run ruff check . --exit-non-zero-on-fix --fix-only
+poetry build -vvv
+poetry run blackbox/ -vvv &
+sleep 5; curl -v -X 'GET' 'http://0.0.0.0:8000/status' -H 'accept: application/json'
 poetry check --lock
