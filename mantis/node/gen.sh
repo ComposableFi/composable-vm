ca11cf4d38a97731fd5611f1dd1df7ac50e85a28
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-08 17:26:44 +0000

    command line

--- mantis/node/gen.sh
+++ mantis/node/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export RUSTFMT=$(which rustfmt)
-cargo progenitor --input=../../schema/mantis_solver_blackbox.json --output=src/mantis_solver_blackbox --name=mantis_solver_blackbox --version="3.0.3"
+cargo progenitor --input=../../schema/mantis_solver_blackbox.json --output=blackbox_rs --name=blackbox_rs --version="3.0.3"
