bf6c89353b52c6affeecd97783307fb4d0c86c68
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-11 16:43:59 +0000

    making separate crate for GLT

--- mantis/node/src/mantis/blackbox/mod.rs
+++ mantis/node/src/mantis/blackbox/mod.rs
@@ -1,29 +1,53 @@
 use blackbox_rs::{prelude::*, types::*, Client};
 /// Given total amount it, order owners and desired out, produce CVM program from and by requesting route
 use cvm_runtime::{
-    shared::{Displayed, XcProgram, XcAddr},
+    shared::{Displayed, XcAddr, CvmInstruction, XcProgram},
     Amount, AssetId, ExchangeId, 
 };
+
+
+/// input batched summarized from users for routing 
+struct BankInput {
+    pub in_asset_id: AssetId,
+    pub in_asset_amount: Displayed<u64>,
+    pub out_asset_id: AssetId,
+    pub order_accounts: Vec<(XcAddr, Amount)>,
+}
+
+impl BankInput {
+    pub fn new(
+        in_asset_id: AssetId,
+        in_asset_amount: Displayed<u64>,
+        out_asset_id: AssetId,
+        order_accounts: Vec<(XcAddr, Amount)>,
+    ) -> Self {
+        Self {
+            in_asset_id,
+            in_asset_amount,
+            out_asset_id,
+            order_accounts,
+        }
+    }
+}
 
 /// `order_accounts` - account of order where to dispatch amounts (part of whole)
 async fn route(
     server: &str,
-    in_asset_id: AssetId,
-    in_asset_amount: Displayed<u64>,
-    out_asset_id: AssetId,
-    order_accounts: Vec<(XcAddr, Amount)>,
+    input : BankInput,
+    
 ) -> XcProgram {
     let blackbox: Client = Client::new(server);
     let route = blackbox
         .simulator_router_simulator_router_get(
-            &InAssetAmount::Variant0(in_asset_amount.0.try_into().expect("in_asset_amount")),
-            &InAssetId::Variant1(in_asset_id.to_string()),
+            &InAssetAmount::Variant0(input.in_asset_amount.0.try_into().expect("in_asset_amount")),
+            &InAssetId::Variant1(input.in_asset_id.to_string()),
             true,
             &OutAssetAmount::Variant0(10),
-            &OutAssetId::Variant1(out_asset_id.to_string().into()),
+            &OutAssetId::Variant1(input.out_asset_id.to_string().into()),
         )
         .await
-        .expect("route found");
-    
-    panic!()
+        .expect("route found")
+        .into_inner()
+        .get(0).expect("at least one route");    
+    CvmInstruction::Spawn { network_id: (), salt: (), assets: (), program: () }
 }
