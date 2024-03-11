64d015cc7c1b6b94c944e8cbd9c3cd82f26a6d29
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-11 11:55:29 +0000

    more typing

--- mantis/node/src/mantis/blackbox/mod.rs
+++ mantis/node/src/mantis/blackbox/mod.rs
@@ -1,17 +1,27 @@
+use blackbox_rs::{prelude::*, types::*, Client};
 /// Given total amount it, order owners and desired out, produce CVM program from and by requesting route
+use cvm_runtime::{
+    shared::{Displayed, XcProgram},
+    Amount, AssetId, ExchangeId,
+};
 
-use cvm_runtime::{shared::XcProgram, AssetId, ExchangeId};
-use blackbox_rs::{prelude::*, Client};
-
-async fn route(server: &str, in_asset_id: AssetId, in_asset_amount: AssetId, out_asset_id: AssetId) -> XcProgram {
+async fn route(
+    server: &str,
+    in_asset_id: AssetId,
+    in_asset_amount: Displayed<u64>,
+    out_asset_id: AssetId,
+) -> XcProgram {
     let blackbox: Client = Client::new(server);
-    let route = blackbox.simulator_router_simulator_router_get(
-        in_asset_id,
-        in_asset_amount,
-        true,
-        out_asset_id,
-        10
-    ).await.unwrap();
-    
+    let route = blackbox
+        .simulator_router_simulator_router_get(
+            &InAssetAmount::Variant0(in_asset_amount.0.try_inot().expect("in_asset_amount")),
+            in_asset_id.to_string().into(),
+            true,
+            out_asset_id.to_string().into(),
+            10.into(),
+        )
+        .await
+        .unwrap();
+
     panic!()
 }
