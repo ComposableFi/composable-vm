4aa7abd580e36b9e0c06af6be3a813ec2ddf8630
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-11 12:33:33 +0000

    started to write dispatch of orders in CVM

--- mantis/node/src/mantis/blackbox/mod.rs
+++ mantis/node/src/mantis/blackbox/mod.rs
@@ -1,27 +1,29 @@
 use blackbox_rs::{prelude::*, types::*, Client};
 /// Given total amount it, order owners and desired out, produce CVM program from and by requesting route
 use cvm_runtime::{
-    shared::{Displayed, XcProgram},
-    Amount, AssetId, ExchangeId,
+    shared::{Displayed, XcProgram, XcAddr},
+    Amount, AssetId, ExchangeId, 
 };
 
+/// `order_accounts` - account of order where to dispatch amounts (part of whole)
 async fn route(
     server: &str,
     in_asset_id: AssetId,
     in_asset_amount: Displayed<u64>,
     out_asset_id: AssetId,
+    order_accounts: Vec<(XcAddr, Amount)>,
 ) -> XcProgram {
     let blackbox: Client = Client::new(server);
     let route = blackbox
         .simulator_router_simulator_router_get(
-            &InAssetAmount::Variant0(in_asset_amount.0.try_inot().expect("in_asset_amount")),
-            in_asset_id.to_string().into(),
+            &InAssetAmount::Variant0(in_asset_amount.0.try_into().expect("in_asset_amount")),
+            &InAssetId::Variant1(in_asset_id.to_string()),
             true,
-            out_asset_id.to_string().into(),
-            10.into(),
+            &OutAssetAmount::Variant0(10),
+            &OutAssetId::Variant1(out_asset_id.to_string().into()),
         )
         .await
-        .unwrap();
-
+        .expect("route found");
+    
     panic!()
 }
