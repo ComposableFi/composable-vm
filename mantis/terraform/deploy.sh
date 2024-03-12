598839e965d4a628755ae64b2b5be39205a39198
rjonczy <robert.jonczy@gmail.com>
2024-03-12 10:40:58 +0100

    added vars for plan and apply

--- mantis/terraform/deploy.sh
+++ mantis/terraform/deploy.sh
@@ -2,6 +2,10 @@
 
 terraform init
 
-terraform plan -var "do_token=${DO_PAT}"
+terraform plan \
+    -var "do_token=${DO_PAT}" \
+    -var-file="terraform.tfvars"
 
-terraform apply -var "do_token=${DO_PAT}"
+terraform apply \
+    -var "do_token=${DO_PAT}" \
+    -var-file="terraform.tfvars"
