974029062bc7f0851e1f18c7d239b38601d302e6
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-12 18:51:55 +0000

    adding builder method

--- crates/cvm-runtime/src/shared.rs
+++ crates/cvm-runtime/src/shared.rs
@@ -1,5 +1,8 @@
+use core::panic;
+
 use crate::{prelude::*, AssetId};
 use cosmwasm_std::{from_json, to_json_binary, Api, Binary, CanonicalAddr, StdError, StdResult};
+use cvm::NetworkId;
 use serde::{de::DeserializeOwned, Serialize};
 
 pub use cvm::shared::*;
@@ -13,6 +16,9 @@ pub type CvmInstruction = crate::Instruction<Vec<u8>, XcAddr, CvmFundsFilter>;
 pub type CvmPacket = crate::Packet<CvmProgram>;
 pub type CvmProgram = crate::Program<Vec<CvmInstruction>>;
 
+
+pub type CvmSpawnRef<'a> = (&'a CvmProgram,&'a CvmFundsFilter);   
+
 impl CvmProgram {
     pub fn new(instructions: Vec<CvmInstruction>) -> Self {
         Self {
@@ -21,16 +27,27 @@ impl CvmProgram {
         }
     }
 
-    pub fn last_spawns(&self) -> Vec<&CvmInstruction> {
+    pub fn will_spawn(&self) -> bool {
         self.instructions
             .iter()
-            .filter_map(|i| 
-                if let CvmInstruction::Spawn { program, .. } = i {
-                    Some(program.last_spawns())
+            .any(|i| matches!(i, CvmInstruction::Spawn { .. }))
+    }
+
+    pub fn last_spawns(&self) -> Vec<CvmSpawnRef> {
+        self.instructions
+            .iter()
+            .filter_map(|i| {
+                if let CvmInstruction::Spawn { program, assets, .. } = i {
+                    if program.will_spawn() {
+                        Some(program.last_spawns())
+                    } else {
+                        Some(vec![(program, assets)])
+                    }
                 } else {
                     None
                 }
-            )
+            })
+            .flatten()
             .collect()
     }
 }
@@ -42,6 +59,15 @@ impl CvmInstruction {
             assets: CvmFundsFilter::one(asset_id.into(), crate::Amount::new(amount, 0)),
         }
     }
+
+    pub fn spawn(network_id: NetworkId, program: CvmProgram ) -> Self {
+        Self::Spawn {
+            network_id,
+            salt: vec![],
+            assets: vec![],
+            program,
+        }
+    }
 }
 
 pub fn to_json_base64<T: Serialize>(x: &T) -> StdResult<String> {
@@ -50,4 +76,19 @@ pub fn to_json_base64<T: Serialize>(x: &T) -> StdResult<String> {
 
 pub fn decode_base64<S: AsRef<str>, T: DeserializeOwned>(encoded: S) -> StdResult<T> {
     from_json::<T>(&Binary::from_base64(encoded.as_ref())?)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{CvmInstruction, CvmProgram};
+
+
+    #[test]
+    pub fn spawn() {
+        let spawn = CvmInstruction::Spawn { network_id: (), salt: (), assets: (), program: () }
+        let program = CvmProgram::new(vec![
+
+        ]);
+    }   
 }
