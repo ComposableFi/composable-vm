de808ab07663cf15b3a9267d701f46ec208e2e34
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-26 21:51:05 +0000

    comment out  constraints

--- mantis/node/src/solver/router.rs
+++ mantis/node/src/solver/router.rs
@@ -7,7 +7,7 @@ use std::{
 };
 
 use cosmrs::tendermint::chain;
-use good_lp::*;
+// use good_lp::*;
 use itertools::*;
 use ndarray::*;
 use prelude::*;
@@ -41,53 +41,53 @@ pub fn populate_chain_dict(chains: &mut HashMap<String, Vec<String>>, center_nod
     }
 }
 
-pub fn solve(
-    all_tokens: Vec<String>,
-    all_cffms: Vec<(String, String)>,
-    reserves: ndarray::Array1<f64>,
-    cfmm_tx_cost: Vec<f64>,
-    fees: Vec<f64>,
-    ibc_pools: u16,
-    origin_token: String,
-    number_of_init_tokens: f64,
-    obj_token: String,
-    force_eta: Vec<f64>,
-) {
-    let count_tokens = all_tokens.len();
-    let count_cffms = all_cffms.len();
-    let mut current_assets = ndarray::Array1::<f64>::from_elem(count_tokens, <_>::default());
+// pub fn solve(
+//     all_tokens: Vec<String>,
+//     all_cffms: Vec<(String, String)>,
+//     reserves: ndarray::Array1<f64>,
+//     cfmm_tx_cost: Vec<f64>,
+//     fees: Vec<f64>,
+//     ibc_pools: u16,
+//     origin_token: String,
+//     number_of_init_tokens: f64,
+//     obj_token: String,
+//     force_eta: Vec<f64>,
+// ) {
+//     let count_tokens = all_tokens.len();
+//     let count_cffms = all_cffms.len();
+//     let mut current_assets = ndarray::Array1::<f64>::from_elem(count_tokens, <_>::default());
 
-    fn find_index<T: PartialEq>(vec: &[T], target: &T) -> usize {
-        vec.iter().position(|x| x == target).unwrap()
-    }
-    current_assets[find_index(&all_tokens, &origin_token)] = number_of_init_tokens;
+//     fn find_index<T: PartialEq>(vec: &[T], target: &T) -> usize {
+//         vec.iter().position(|x| x == target).unwrap()
+//     }
+//     current_assets[find_index(&all_tokens, &origin_token)] = number_of_init_tokens;
 
-    let mut problem = ProblemVariables::new();
-    let mut main_expression = Expression::default();
-    let mut constraints: Vec<Constraint> = vec![];
+//     let mut problem = ProblemVariables::new();
+//     let mut main_expression = Expression::default();
+//     let mut constraints: Vec<Constraint> = vec![];
 
-    // Build variables
+//     // Build variables
 
-    let mut variables = variables!();
+//     let mut variables = variables!();
 
-    let mut A: Vec<Vec<Vec<f64>>> = vec![];
+//     let mut A: Vec<Vec<Vec<f64>>> = vec![];
 
-    for cfmm in all_cffms.iter() {
-        let n_i = 2;
-        let mut A_i = vec![vec![0.0, 0.0]; count_tokens];
-        A_i[find_index(&all_tokens, &cfmm.0)][0] = 1.0;
-        A_i[find_index(&all_tokens, &cfmm.1)][1] = 1.0;
-        A.push(A_i);
-    }
+//     for cfmm in all_cffms.iter() {
+//         let n_i = 2;
+//         let mut A_i = vec![vec![0.0, 0.0]; count_tokens];
+//         A_i[find_index(&all_tokens, &cfmm.0)][0] = 1.0;
+//         A_i[find_index(&all_tokens, &cfmm.1)][1] = 1.0;
+//         A.push(A_i);
+//     }
 
-    let mut deltas: Vec<Vec<VariableDefinition>> = vec![];
-    let mut lambdas: Vec<Vec<VariableDefinition>> = vec![];
-    // Binary value, indicates tx or not for given pool
-    let mut eta: Vec<VariableDefinition> = vec![];
+//     let mut deltas: Vec<Vec<VariableDefinition>> = vec![];
+//     let mut lambdas: Vec<Vec<VariableDefinition>> = vec![];
+//     // Binary value, indicates tx or not for given pool
+//     let mut eta: Vec<VariableDefinition> = vec![];
 
-    for _ in all_cffms {
-        deltas.push(vec![variable(), variable()]);
-        lambdas.push(vec![variable(), variable()]);
-        eta.push(variable());
-    }
-}
+//     for _ in all_cffms {
+//         deltas.push(vec![variable(), variable()]);
+//         lambdas.push(vec![variable(), variable()]);
+//         eta.push(variable());
+//     }
+// }
