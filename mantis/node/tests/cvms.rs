145954416e51ba5293f3c6a8fd5185409e1238b3
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-04-11 12:41:54 +0100

    outpost

--- mantis/node/tests/cvms.rs
+++ mantis/node/tests/cvms.rs
@@ -1,36 +1,33 @@
-
-
-use std::ops::Add;
-
-use cw_orch::prelude::*;
-use cw_orch::interface;
-
-use cw_multi_test::App;
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Empty};
+use cw_multi_test::{App, Contract, ContractWrapper};
+// use cw_orch::prelude::*;
+// use cw_orch::interface;
 
 #[test]
 fn cvm_devnet_case() {
-    let centauri = App::default();
-    let _osmosis = App::default();
-    let cw_mantis_order_wasm = ContractWrapper::new(
+    let mut centauri = App::default();
+    let mut _osmosis = App::default();
+    let cw_mantis_order_wasm = (ContractWrapper::new(
         cw_mantis_order::entry_points::execute,
         cw_mantis_order::entry_points::instantiate,
         cw_mantis_order::entry_points::query,
-    );
+    ));
 
-    let cw_cvm_outpost_wasm = ContractWrapper::new(
+    let cw_cvm_outpost_wasm = (ContractWrapper::new(
         cw_cvm_outpost::contract::execute::execute,
         cw_cvm_outpost::contract::instantiate,
         cw_cvm_outpost::contract::query::query,
-    );
+    ));
 
-    let cw_cvm_executor_wasm = ContractWrapper::new(
+    let cw_cvm_executor_wasm = (ContractWrapper::new(
         cw_cvm_executor::contract::execute,
         cw_cvm_executor::contract::instantiate,
         cw_cvm_executor::contract::query,
-    );
+    ));
+
+    let cw_mantis_order_code_id = centauri.store_code(Box::new(cw_mantis_order_wasm));
+    let cw_cvm_outpost_code_id = centauri.store_code(Box::new(cw_cvm_outpost_wasm));
+    let cw_cvm_executor_code_id = centauri.store_code(Box::new(cw_cvm_executor_wasm));
 
     let sender = Addr::unchecked("juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y");
 }
-
-
